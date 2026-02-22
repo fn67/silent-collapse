3ee903a786a696f877ad8e3a4d02d91480120b15
fn67 <arun367@hotmail.com>
2026-02-23 01:31:44 +0530

    Update particles

--- src/config/particleConfig.js
+++ src/config/particleConfig.js
@@ -1,33 +1,69 @@
 const particleConfig = {
-  particles: {
-    number: { value: 40 },
-    color: { value: "#ffffff" },
-    opacity: {
-      value: 0.3,
-      random: true,
-    },
-    size: {
-      value: 1.5,
-      random: true,
-    },
-    move: {
-      enable: true,
-      speed: 0.3,
-      direction: "none",
-      random: true,
-      straight: false,
-    },
-    links: {
-      enable: false,
-    },
-  },
-  interactivity: {
-    events: {
-      onHover: { enable: false },
-      onClick: { enable: false },
-    },
-  },
-  detectRetina: true,
-};
+      background: {
+        color: {
+          value: "#000",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.2,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    };
 
 export default particleConfig;
